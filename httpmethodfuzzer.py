f4a3b0bdc368a837cffa12afee6d079cbcdd2d06
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2019-01-03 19:00:38 -0600

    Add files via upload

--- httpmethodfuzzer.py
+++ httpmethodfuzzer.py
@@ -1,27 +1,26 @@
 #!/bin/python
-import putupload_options
 import requests
 import os
 import argparse
 
 #OPTIONS!
-parser = argparse.ArgumentParser(description='http methods fuxxer')
+parser = argparse.ArgumentParser(description='Brute-Force fuzzer for all methods and options, regexes for a reflection in the response')
 parser.add_argument('--file',
                                  dest    = 'file',
                                  action  = "store" ,
                                  default = 'simplebackdoor.php' ,
-                                 help    = "The file you are wanting to upload" )
+                                 help    = "The file you are wanting to upload or payload you wish to test" )
 parser.add_argument('--cookie',
                                  dest    = 'cookie',
                                  action  = "store" ,
                                  default = 'cookie.txt' ,
-                                 help    = "this file is a cookie jar! Only big enough for one cookie! Put a cookie recipie you wish to send along with ... y'kow... whatever" )
+                                 help    = "this file is a cookie jar! Only big enough for one cookie! Put a cookie recipie you wish to send along with ... y'know... whatever" )
 
 parser.add_argument('--target',
                                  dest    = 'target',
                                  action  = "store" ,
-                                 default = '127.0.0.1' ,
-                                 help    = "TARGET to attack, defaults to localhost" )
+                                 default = 'http://127.0.0.1' ,
+                                 help    = "TARGET to attack, defaults to localhost. Don't forget the http://" )
 
 parser.add_argument('--xforwardedfor',
                                  dest    = 'xforward',
@@ -34,25 +33,29 @@ parser.add_argument('--xforwardedip',
                                  default = '127.0.0.1' ,
                                  help    = "Sets the X-Forward-IP header to... something" )
 
-parser.add_argument('--methhammer',
-                                 dest    = 'methhammer',
-                                 action  = "store_true" ,
-                                 default = False ,
-                                 help    = "Brute-Force fuzzer for all methods and options, regexes for a reflection in the response" )
 
 arguments = parser.parse_args()
 
 class  Uploader:
     """required arguments are the url, method, and filename."""
     def __init__(self):
-        self.header         = {'Content-type' : mimetype}
+        self.header         = {}
         self.uploadfilename = uploadfilename
         self.fileobj        = openfile(self.uploadfilename)
         self.url            = arguments.target
         self.xforward       = arguments.xforward
         self.xforwardip     = arguments.xforwardedip
+        self.useragent      = 'Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101  Firefox/28.0'
         self.mimetype       = ''
-        self.mimelist       = mimelist = ['text/plain','application/octet-stream','text/css','text/html','text/javascript','image/gif','image/jpeg','image/png','image/svg-xml']
+        self.mimelist       = mimelist = ['text/plain',
+                                        'application/octet-stream',
+                                        'text/css',
+                                        'text/html',
+                                        'text/javascript',
+                                        'image/gif',
+                                        'image/jpeg',
+                                        'image/png',
+                                        'image/svg-xml']
         self.methodlist     = ['get', 'post','put','head','delete', 'patch','options']
         self.methodholder   = ''
 
@@ -60,7 +63,9 @@ class  Uploader:
             self.header.update({'X-Forwarded-For' : self.xforward})
         if self.xforwardip != None:
             self.header.update({'X-Forwarded-IP' : self.xforward})
-        if arguments.cookiejar
+        if arguments.cookiejar != None:
+            self.cookie = openfile(self.cookiejar) #pulling a cookie from the jar!
+
     def openfile(filename):
         try:
             fileobject = open(arguments.file, 'rb')
@@ -70,15 +75,34 @@ class  Uploader:
             print(error)
 
     def fuzzer(target, content):
-        try:
-            for each in self.methodlist:
-                self.methodholder = each
-                for each in self.mimelist:
-                    fuzzresult = requests.request(method = self.methodholder, data = content, headers = {'Content-type' : each}, url = target )
-                    print('status code : ' + upload.status_code)
-            else:
-                None
-        except KeyboardInterrupt:
+        if arguments.cookiejar != None:
+            try:
+                for each in self.methodlist:
+                    self.methodholder = each
+                    for each in self.mimelist:
+                        fuzzresult = requests.request(method    = self.methodholder,
+                                                        data    = content,
+                                                        headers = {'Content-type' : each},
+                                                        url     = target,
+                                                        cookies = self.cookie)
+                        print('status code : ' + upload.status_code)
+                        if fuzzresult.status_code == 200:
+                            fuzzanalyzer(fuzzresult.content)
+            except KeyboardInterrupt:
+                print('KEYBOARD SIGNALS')
+        elif arguments.cookiejar == None:
+            try:
+                for each in self.methodlist:
+                    self.methodholder = each
+                    for each in self.mimelist:
+                        fuzzresult = requests.request(method    = self.methodholder,
+                                                        data    = content,
+                                                        headers = self.header.update({'Content-type' : each}),
+                                                        url     = target )
+                        print('status code : ' + fuzzresult.status_code)
+                        if fuzzresult.status_code == 200:
+                            fuzzanalyzer(fuzzresult.content)
+            except KeyboardInterrupt:
                 print('KEYBOARD SIGNALS')
 
     def listener():
